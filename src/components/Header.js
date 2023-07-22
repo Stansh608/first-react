b46a834f6fccedcc779ce437bdf0ef519fe69bff
Stansh608 <stanleymunene56@gmail.com>
2023-07-22 10:12:22 +0300

    props

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = () => {
   return (
     <div>
-      <h1>This is the Header Component here</h1>
+      <h0>This is the Header Component here</h0>
     </div>
   )
 }
